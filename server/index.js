abb9b27e9498455f0ba2afc8441b69308e2c751a
Anand <131908427+anandj1@users.noreply.github.com>
2025-02-09 02:37:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://community-crisis-full-stack.onrender.com'],
+  origin: ['http://localhost:5173', 'https://community-crisis-full-stack.onrender.com','https://crisisconnected.netlify.app/],
   credentials: true
 }));
 
